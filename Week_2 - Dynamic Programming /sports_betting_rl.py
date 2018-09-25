367e1e7c5fb12818a33aacd88cf31a10f31ac40b
ajoseph12 <allwyn_don32@yahoo.com>
2018-09-26 01:45:32 +0200

    sports_bet command line call

--- Week_2 - Dynamic Programming /sports_betting_rl.py
+++ Week_2 - Dynamic Programming /sports_betting_rl.py
@@ -25,85 +25,109 @@ Learnings
 While the Value Iteration (Dynamic Programming Algorithm) does help one arrive at an optimal policy, one can't use it
 with events having numerous states - it could prove to be computationally expensive - or an imperfect environment model . 
 Neverthless a useful thought exercise.
+
 """
 
 import matplotlib.pyplot as plt
 
 
-gamma = 1 # discount factor
-ph = 0.4 # probability of home team winning
-num_states = 100 # number of available states
-reward = [0]*101 # list for storing reward value
-reward[100] = 1
-theta = 0.000000001 # threshold for comparing the difference"
-temp = []
-value = [0]*101 # list to store value function 
-policy = [0]*101 # List to store the amount of bet that gives the max reward
+class sports_bet(object):
 
 
-def reinforcement_learning():
+	def __init__(self, gamma, num_states, theta, ph, plot = False):
 
-	delta = 1
-	
-	while delta > theta:
-
-		delta = 0
-
-		for state in range(1, num_states): # Looping over all the states i.e the money in hand for a current episode
-
-			old_val = value[state]
-			bellman_equation(state)
-			diff = abs(old_val - value[state])
-			delta = max(diff,delta)
-
-	
-	print(value)
-	print(policy)
-	
-
-def bellman_equation(state):
-
-	optimal_action_val = 0
-
-	for bet in range(0, min(state, 100-state)+1):
-
-		win = state + bet
-		lose = state - bet
-		# action value is the sum of immediate reward + gamma(summation(prob(ss',a) * value(s')))
-		# here the immediate reward depends on the end state, hence its expected value is taken
-		expected_reward = ph*reward[win] + (1-ph)*reward[lose]
-		expected_value = ph*value[win] + (1-ph)*value[lose]
-		action_val = expected_reward + gamma * expected_value
+		self.gamma = gamma # discount factor
+		self.num_states = num_states # number of available states
+		self.theta = theta # threshold for comparing the difference of state values calculated"
+		self.ph = ph # probability of home team winning
 		
-		if action_val > optimal_action_val:
+		self.reward = [0]*(self.num_states+1) # list for storing reward value
+		self.reward[100] = 1
+
+		self.value = [0]*(self.num_states+1) # list to store value function 
+		self.policy = [0]*(self.num_states+1) # List to store the amount of bet that gives the max reward
+
+		self._reinforcement_learning()
+
+		if plot == True : self._plot()
+
+	
+
+	def _reinforcement_learning(self):
+
+		delta = 1
+		
+		while delta > self.theta:
+
+			delta = 0
+
+			for state in range(1, self.num_states): # Looping over all the states i.e the money in hand for a current episode
+
+				old_val = self.value[state]
+				self._bellman_equation(state)
+				diff = abs(old_val - self.value[state])
+				delta = max(diff,delta)
+
+		
+		print(self.value)
+		print(self.policy)
+		
+
+	
+	def _bellman_equation(self, state):
+
+		optimal_action_val = 0
+
+		for bet in range(0, min(state, 100-state)+1):
+
+			win = state + bet
+			lose = state - bet
+			# action value is the sum of immediate reward + gamma(summation(prob(ss',a) * value(s')))
+			# here the immediate reward depends on the end state, hence its expected value is taken
+			expected_reward = self.ph*self.reward[win] + (1-self.ph)*self.reward[lose]
+			expected_value = self.ph*self.value[win] + (1-self.ph)*self.value[lose]
+			action_val = expected_reward + gamma * expected_value
 			
-			optimal_action_val = action_val
-			value[state] = action_val
-			policy[state] = bet
+			if action_val > optimal_action_val:
+				
+				optimal_action_val = action_val
+				self.value[state] = action_val
+				self.policy[state] = bet
 
-reinforcement_learning()
+	
 
+	def _plot(self):
 
-# Value Plot 
-plt.plot(value[:100])
-plt.ylabel('State-value')
-plt.xlabel('States / Capital')
-plt.show()
-
-
-# Policy Plot 
-plt.plot(policy[:100])
-plt.ylabel('Policy / Bets')
-plt.xlabel('States / Capital')
-plt.show()
+		# Value Plot 
+		plt.plot(self.value[:100])
+		plt.ylabel('State-value')
+		plt.xlabel('States / Capital')
+		plt.show()
 
 
-
-
-
+		# Policy Plot 
+		plt.plot(self.policy[:100])
+		plt.ylabel('Policy / Bets')
+		plt.xlabel('States / Capital')
+		plt.show()
 
 
 
+
+def main(gamma, num_states, theta, ph, plot):
+
+	bet = sports_bet(gamma, num_states, theta, ph, plot)
+
+
+if __name__ == '__main__':
+
+	gamma = 1 # discount factor
+	num_states = 100 # number of available states
+	theta = 0.000000001 # threshold for comparing the difference of state values calculated"
+	ph = 0.4 # probability of home team winning
+	plot = True
+	
+	main(gamma, num_states, theta, ph, plot)
 
 
 
